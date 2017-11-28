0f069341e47b60b27c96cf12b413a233a06855d6
vonbv25 <vonbv_25@yahoo.com.ph>
2017-11-28 21:23:58 +0800

    To Boss Jay
    Boss pa code Review po

--- src/Inventory/Domain/Catalog.cs
+++ src/Inventory/Domain/Catalog.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+
 namespace Domain
 {
-    public class Catalog
+    public class Catalog : IStateContainer<CatalogState>
     {
-        
+        private readonly string _name;
+
+        private readonly List<Product> _productList;
+
+        public CatalogState GetCurrentState()
+        {
+            return new CatalogState(_name, _productList);
+        }
+    }
+
+    public class CatalogState
+    {
+        public CatalogState(string name, List<Product> productList)
+        {
+            Name        = name;
+            ProductList = productList;
+        }
+
+        public string Name { get; private set; }
+        public List<Product> ProductList { get; private set; }
     }
 }
--- src/Inventory/Domain/Inventory.cs
+++ src/Inventory/Domain/Inventory.cs
@@ -1,7 +1,24 @@
 namespace Domain
 {
-    public class Inventory
+    public class Inventory : IStateContainer<InventoryState>
     {
-        
+        private readonly string _name;
+        private readonly List<Catalog> _catalogList;
+        public InventoryState GetCurrentState()
+        {
+            return new InventoryState(_name, _catalogList);
+        }
+    }
+
+    public class InventoryState
+    {
+        public InventoryState(string name, List<Catalog> catalogList)
+        {
+            Name = name;
+            CatalogList = catalogList;
+        }
+
+        public object Name { get; private set; }
+        public object CatalogList { get; private set; }
     }
 }
